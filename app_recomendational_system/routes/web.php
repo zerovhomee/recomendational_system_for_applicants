0ef4b2b89ca60ff285cbdf7b6efbbb5cd7f94689
Andrey Stekachev <zerov.live@gmail.com>
2025-05-08 21:30:34 +0500

    Created files for routing and showing pages

--- app_recomendational_system/routes/web.php
+++ app_recomendational_system/routes/web.php
@@ -9,3 +9,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/test', [App\Http\Controllers\TestShowController::class, 'test'])->name('test');
+
+Route::get('/result', [App\Http\Controllers\ResultController::class, 'result'])->name('result');
+
+Route::get('/user/{user}', [App\Http\Controllers\UserShowController::class, 'user'])->name('user');
