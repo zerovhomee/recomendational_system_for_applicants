5aea78a028dea35497c655fc7850cf89ecaf013e
LEV <leva.shepeta@mail.ru>
2025-06-13 14:07:48 +0500

    Changed design for main page and make auth/reg pages

--- app_recomendational_system/design_raw/scripts/script_page_1.js
+++ app_recomendational_system/design_raw/scripts/script_page_1.js
@@ -1,16 +1,16 @@
 const swiper = new Swiper(".swiper", {
-  loop: true,
-  centeredSlides: true,
-  slidesPerView: 2,
-  spaceBetween: 50,
-  speed: 800,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: 1,
+    spaceBetween: 50,
+    speed: 800,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 });
